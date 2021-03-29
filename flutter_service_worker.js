419c1ba157ce9d08b222cd01440a2c838fc60d49
norbertkross <norbertaberor@gmail.com>
2021-03-29 00:42:39 +0000

    added norbertaberor portfolio1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -55,8 +55,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "d91b6644756e27f086fc86ba2e81e158",
-"/": "d91b6644756e27f086fc86ba2e81e158",
+"index.html": "cb4e09af7df47fde98577a3d9a30d40d",
+"/": "cb4e09af7df47fde98577a3d9a30d40d",
 "main.dart.js": "d92b3d6e51b577809b0a48b6a260e9fe",
 "manifest.json": "62de9cc5c327b044021aac4f66c64e12",
 "version.json": "ad71d2eb44c54a7963dce4a31dd6aa90"
