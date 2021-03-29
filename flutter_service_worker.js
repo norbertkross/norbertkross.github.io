b1fbf059f3603fa041b29a888d7df7717d48cf0d
norbertkross <norbertaberor@gmail.com>
2021-03-29 15:03:26 +0000

    Used a slide over widget

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -55,9 +55,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "cb4e09af7df47fde98577a3d9a30d40d",
-"/": "cb4e09af7df47fde98577a3d9a30d40d",
-"main.dart.js": "d92b3d6e51b577809b0a48b6a260e9fe",
+"index.html": "4b6dbd07be8c5cc09537813b595ed416",
+"/": "4b6dbd07be8c5cc09537813b595ed416",
+"main.dart.js": "00873a8c9a79d5c30dfb169bede637c6",
 "manifest.json": "62de9cc5c327b044021aac4f66c64e12",
 "version.json": "ad71d2eb44c54a7963dce4a31dd6aa90"
 };
